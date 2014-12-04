1f17bec7dea2af3d4342ca0c4ce14e786f1c2dc1
Mike Sconzo <sooshie@gmail.com>
2014-12-03 23:22:27 -0600

    added sig extraction, and fixed some code issues

--- packerid.py
+++ packerid.py
@@ -22,7 +22,7 @@
 # 2007-10-25 - add -V switch to print out version number
 # 2009-05-15 - added some error handling as recommended by Joerg Hufschmidt
 #
-# 2014-12-03 - incremented to 1.5, moved over to Argument Parser and created terse output - sooshie@gmail.com
+# 2014-12-03 - incremented to 1.5, sig extraction, moved over to Argument Parser and created terse output - sooshie@gmail.com
 
 import pefile
 import peutils
@@ -30,101 +30,136 @@ import time
 import sys
 from argparse import ArgumentParser
 
-version = "1.5"
-parser = ArgumentParser(description="Used to check PEid databases against files in Python")
-parser.add_argument("-a", "--all",
-                    dest="show_all", help="show all PE info", default=False, action="store_true")
-parser.add_argument("-D", "--database",
-                    dest="alt_db", help="use alternate signature database DB", metavar="DB")
-parser.add_argument("-m", "--all-matches",
-                    dest="show_matches", help="show all signature matches", default=False, action="store_true")
-parser.add_argument("-t", "--terse",
-                    dest="terse", help="give a short listing of various PE properties", default=False, action="store_true")
-parser.add_argument("-V", "--version",
-                    dest="version", help="show version number", default=False, action="store_true")
-parser.add_argument("files", nargs='+', help='Files to analyze')
+def get_sig(filename, pe, extract):
+    print "Digital Signature"
+    address = pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']].VirtualAddress
+    size = size = pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']].Size
 
-args = parser.parse_args()
+    if address == 0:
+        print "\tNone"
+        return
 
-if args.version:
-    print "Packerid.py version ",version,"\n Copyright (c) 2007, Jim Clausing, forked by Sconzo"
-    sys.exit(0)
+    if extract:
+        with open(filename + ".der", 'wb+') as out:
+            offset = address+8
+            out.write(pe.write()[offset:offset+size])
+            print "\t%s %s %s.der" %(hex(address), size, filename)
+    else:
+        print "\t%s %s" %(hex(address), size)
 
-if args.alt_db:
-    signatures = peutils.SignatureDatabase(args.alt_db)
-else:
-    signatures = peutils.SignatureDatabase('/usr/local/etc/userdb.txt')
-
-for file in vars(args)['files']:
-    try:
-        pe = pefile.PE(file)
-    except Exception as e:
-        print "[*] Error with %s - %s" %(file, str(e))
-        continue
-
-    if args.show_all|args.show_matches:
-        matches = signatures.match_all(pe, ep_only = True)
-        t = []
-        if len(matches) == 0:
-            print "\tNone"
-        else:
-            t = set()
-            for m in matches:
-                t.add(m[0])
-            print "\t" + ", ".join(t)
-    elif not args.terse:
-        matches = signatures.match(pe, ep_only = True)
-        print matches[0]
-
-    if args.show_all:
-        print pe.dump_info()
-
-    if args.terse:
-        print "PEid"
-        matches = signatures.match_all(pe, ep_only = True)
-        if len(matches) == 0:
-            print "\tNone"
-        else:
-            t = set()
-            for m in matches:
-                t.add(m[0])
-            print "\t" + ", ".join(t)
-
-        print "Entry Point Address\n\t%s" %(hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint))
-        print "Image Base Address\n\t%s" %(hex(pe.OPTIONAL_HEADER.ImageBase))
-        print "Linker Version\n\t%d.%d" %(pe.OPTIONAL_HEADER.MajorLinkerVersion,pe.OPTIONAL_HEADER.MinorLinkerVersion)
-        print "OS Version\n\t%d.%d" %(pe.OPTIONAL_HEADER.MajorOperatingSystemVersion,pe.OPTIONAL_HEADER.MinorOperatingSystemVersion)
-        print "Machine"
-        if pe.FILE_HEADER.Machine == 0x14c: print "\tx86"
-        elif pe.FILE_HEADER.Machine == 0x14d: print "\t486"
-        elif pe.FILE_HEADER.Machine == 0x14e: print "\tPentium"
-        elif pe.FILE_HEADER.Machine == 0x0200: print "\tAMD64 only"
-        elif pe.FILE_HEADER.Machine == 0x8664: print "\t64b"
-        else: print "Unknown"
+def main():
+    version = "1.5"
+    parser = ArgumentParser(description="Used to check PEid databases against files in Python")
+    parser.add_argument("-a", "--all",
+                        dest="show_all", help="show all PE info", default=False, action="store_true")
+    parser.add_argument("-D", "--database",
+                        dest="alt_db", help="use alternate signature database DB", metavar="DB")
+    parser.add_argument("-m", "--all-matches",
+                        dest="show_matches", help="show all signature matches", default=False, action="store_true")
+    parser.add_argument("-t", "--terse",
+                        dest="terse", help="give a short listing of various PE properties", default=False, action="store_true")
+    parser.add_argument("-V", "--version",
+                        dest="version", help="show version number", default=False, action="store_true")
+    parser.add_argument("-e" "--extract-sig",
+                        dest="extract_sig", help="extracts the digital signature", default=False, action="store_true")
+    parser.add_argument("files", nargs='+', help='Files to analyze')
+    
+    args = parser.parse_args()
+    
+    if args.version:
+        print "Packerid.py version ",version,"\n Copyright (c) 2007, Jim Clausing, forked by Sconzo"
+        sys.exit(0)
+    
+    if args.alt_db:
+        signatures = peutils.SignatureDatabase(args.alt_db)
+    else:
+        signatures = peutils.SignatureDatabase('/usr/local/etc/userdb.txt')
+    
+    for file in vars(args)['files']:
         try:
-            print 'Compile Time\n\t%s UTC' %(time.asctime(time.gmtime(pe.FILE_HEADER.TimeDateStamp)))
-        except ValueError, e:
-            print "Compile Time\n\tInvalid Time"
-        print "CheckSum"
-        try:
-            print "\t%s" %(pe.IMAGE_OPTIONAL_HEADER.CheckSum)
-        except AttributeError as e:
-            print "\tNone"
-        print "Sections"
-        for section in pe.sections:
-              print "\t%s %s %s %s" %(section.Name, hex(section.VirtualAddress), hex(section.Misc_VirtualSize), section.SizeOfRawData )
-        print "Imports"
-        try:
-            for entry in pe.DIRECTORY_ENTRY_IMPORT:
-                  print "\t" + entry.dll
-                  for imp in entry.imports:
-                      print '\t\t', hex(imp.address), imp.name
-        except AttributeError as e:
-            print "\tNone"
-        print "Exports"
-        try:
-            for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-                print "%s %s" %(exp.name, exp.ordinal)
-        except AttributeError as e:
-            print "\tNone"
+            pe = pefile.PE(file)
+        except Exception as e:
+            print "[*] Error with %s - %s" %(file, str(e))
+            continue
+    
+        if args.show_all|args.show_matches:
+            matches = signatures.match_all(pe, ep_only = True)
+            t = []
+            if matches == None or len(matches) == 0:
+                print "\tNone"
+            else:
+                t = set()
+                for m in matches:
+                    t.add(m[0])
+                print "\t" + ", ".join(t)
+        elif not args.terse:
+            matches = signatures.match(pe, ep_only = True)
+            if matches == None:
+                print "\tNone"
+            else:
+                print matches[0]
+    
+        if args.show_all:
+            print pe.dump_info()
+    
+        if args.terse:
+            print "PEid"
+            matches = signatures.match_all(pe, ep_only = True)
+            if matches == None or len(matches) == 0:
+                print "\tNone"
+            else:
+                t = set()
+                for m in matches:
+                    t.add(m[0])
+                print "\t" + ", ".join(t)
+    
+            print "Entry Point Address\n\t%s" %(hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint))
+            print "Image Base Address\n\t%s" %(hex(pe.OPTIONAL_HEADER.ImageBase))
+            print "Linker Version\n\t%d.%d" %(pe.OPTIONAL_HEADER.MajorLinkerVersion,pe.OPTIONAL_HEADER.MinorLinkerVersion)
+            print "OS Version\n\t%d.%d" %(pe.OPTIONAL_HEADER.MajorOperatingSystemVersion,pe.OPTIONAL_HEADER.MinorOperatingSystemVersion)
+            print "Machine"
+            if pe.FILE_HEADER.Machine == 0x14c: print "\tx86"
+            elif pe.FILE_HEADER.Machine == 0x14d: print "\t486"
+            elif pe.FILE_HEADER.Machine == 0x14e: print "\tPentium"
+            elif pe.FILE_HEADER.Machine == 0x0200: print "\tAMD64 only"
+            elif pe.FILE_HEADER.Machine == 0x8664: print "\t64b"
+            else: print "Unknown"
+            try:
+                print 'Compile Time\n\t%s UTC' %(time.asctime(time.gmtime(pe.FILE_HEADER.TimeDateStamp)))
+            except ValueError, e:
+                print "Compile Time\n\tInvalid Time"
+            print "CheckSum"
+            try:
+                print "\t%s" %(pe.IMAGE_OPTIONAL_HEADER.CheckSum)
+            except AttributeError as e:
+                print "\tNone"
+            print "Sections"
+            for section in pe.sections:
+                name = ""
+                # occasionally a nul sneaks in, don't print from the nul to eos
+                if "\0" in section.Name:
+                    nul = section.Name.index("\0")
+                    name = section.Name[:nul]
+                print "\t%s %s %s %s" %(name, hex(section.VirtualAddress), hex(section.Misc_VirtualSize), section.SizeOfRawData )
+            print "Imports"
+            try:
+                for entry in pe.DIRECTORY_ENTRY_IMPORT:
+                    print "\t" + entry.dll
+                    for imp in entry.imports:
+                        print '\t\t%s %s' %(hex(imp.address), (imp.name if imp.ordinal == None else "("+str(imp.ordinal)+")"))
+            except AttributeError as e:
+                print "\tNone"
+            print "Exports"
+            try:
+                if len(pe.DIRECTORY_ENTRY_EXPORT.symbols) > 0:
+                    for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+                        print '\t\t%s %s (%s)' %(hex(exp.address), exp.name, exp.ordinal)
+                else:
+                    print "\tNone"
+            except AttributeError as e:
+                print "\tNone"
+            get_sig(file, pe, args.extract_sig)
+        return
 
+if __name__ == "__main__":
+    main()
